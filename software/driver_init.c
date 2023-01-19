9a51f93e9096147a0e8049eb8a4161d37c3eefb8
Leon Rinkel <leon@rinkel.me>
2023-01-19 16:25:28 +0100

    add software from atmel start

---
+++ software/driver_init.c
@@ -0,0 +1,145 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+struct can_async_descriptor CAN_0;
+
+struct usart_sync_descriptor TARGET_IO;
+
+void TARGET_IO_PORT_init(void)
+{
+
+	gpio_set_pin_function(PA14, PINMUX_PA14C_SERCOM2_PAD2);
+
+	gpio_set_pin_function(PA15, PINMUX_PA15C_SERCOM2_PAD3);
+}
+
+void TARGET_IO_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_CORE, CONF_GCLK_SERCOM2_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_SLOW, CONF_GCLK_SERCOM2_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_mclk_set_APBCMASK_SERCOM2_bit(MCLK);
+}
+
+void TARGET_IO_init(void)
+{
+	TARGET_IO_CLOCK_init();
+	usart_sync_init(&TARGET_IO, SERCOM2, (void *)NULL);
+	TARGET_IO_PORT_init();
+}
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+void CAN_0_PORT_init(void)
+{
+
+	gpio_set_pin_function(PA25, PINMUX_PA25G_CAN0_RX);
+
+	gpio_set_pin_function(PA24, PINMUX_PA24G_CAN0_TX);
+}
+/**
+ * \brief CAN initialization function
+ *
+ * Enables CAN peripheral, clocks and initializes CAN driver
+ */
+void CAN_0_init(void)
+{
+	hri_mclk_set_AHBMASK_CAN0_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, CAN0_GCLK_ID, CONF_GCLK_CAN0_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	can_async_init(&CAN_0, CAN0);
+	CAN_0_PORT_init();
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PA05
+
+	// Set pin direction to input
+	gpio_set_pin_direction(BTN, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(BTN,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(BTN, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA09
+
+	gpio_set_pin_level(RX_ACT,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(RX_ACT, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(RX_ACT, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA10
+
+	gpio_set_pin_level(TX_ACT,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(TX_ACT, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(TX_ACT, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA17
+
+	// Set pin direction to input
+	gpio_set_pin_direction(CAN_ERR, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(CAN_ERR,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(CAN_ERR, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA18
+
+	gpio_set_pin_level(CAN_EN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(CAN_EN, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(CAN_EN, GPIO_PIN_FUNCTION_OFF);
+
+	TARGET_IO_init();
+
+	delay_driver_init();
+	CAN_0_init();
+}
